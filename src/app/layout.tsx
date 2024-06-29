dbee2facf86f829fe2f0b888eb65432bf99e8a14
nico-schonfeld <nicoschonfeld88@gmail.com>
2024-06-29 10:48:36 -0300

    update logo nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import { ThemeProvider } from "@/components/theme-provider";
 import ReduxProvider from "@/redux/provider";
 
