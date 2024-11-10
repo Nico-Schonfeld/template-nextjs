805cd50159b7e6c79022069bfd787a4afb85d584
Nico Schönfeld <nicoschonfeld88@gmail.com>
2024-11-10 18:24:50 -0300

    Add template-nextjs-15 folder

--- src/utils/validateObjectState/validateObjectState.ts
+++ src/utils/validateObjectState/validateObjectState.ts
@@ -1,4 +1,8 @@
-export const validateAllFieldsFilled = (state: any) => {
+interface State {
+  [key: string]: unknown; // Puedes ajustar esto según los tipos específicos de tus valores
+}
+
+export const validateAllFieldsFilled = (state: State): boolean => {
   // Validar que todos los campos del estado están llenos
   const allFieldsFilled = Object.values(state).every(
     (value) => value !== null && value !== ""
